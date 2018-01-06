9a5a84444f767fdeff409572830752c4196e3d67
高科 <gaoke@gaokedeMacBook-Pro.local>
2018-01-06 14:18:23 +0800

    add 日志流水

---
+++ app/src/main/java/org/kk/redpacktools/utils/TimeUtils.java
@@ -0,0 +1,41 @@
+package org.kk.redpacktools.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeUtils {
+
+    private static final int YEAR = 365 * 24 * 60 * 60;// 年
+    private static final int MONTH = 30 * 24 * 60 * 60;// 月
+    private static final int DAY = 24 * 60 * 60;// 天
+    private static final int HOUR = 60 * 60;// 小时
+    private static final int MINUTE = 60;// 分钟
+
+    /**
+     * 根据时间戳获取描述性时间，如3分钟前，1天前
+     *
+     * @param timestamp 时间戳 单位为毫秒
+     * @return 时间字符串
+     */
+    public static String getDescriptionTimeFromTimestamp(long timestamp) {
+        long currentTime = System.currentTimeMillis();
+        long timeGap = (currentTime - timestamp) / 1000;// 与现在时间相差秒数
+        System.out.println("timeGap: " + timeGap);
+        String timeStr = null;
+        if (timeGap > YEAR) {
+            timeStr = timeGap / YEAR + "年前";
+        } else if (timeGap > MONTH) {
+            timeStr = timeGap / MONTH + "个月前";
+        } else if (timeGap > DAY) {// 1天以上
+            timeStr = timeGap / DAY + "天前";
+        } else if (timeGap > HOUR) {// 1小时-24小时
+            timeStr = timeGap / HOUR + "小时前";
+        } else if (timeGap > MINUTE) {// 1分钟-59分钟
+            timeStr = timeGap / MINUTE + "分钟前";
+        } else {// 1秒钟-59秒钟
+            timeStr = "刚刚";
+        }
+        return timeStr;
+    }
+
+}
--- app/src/main/java/org/kk/redpacktools/view/LogActivity.java
+++ app/src/main/java/org/kk/redpacktools/view/LogActivity.java
@@ -1,14 +1,20 @@
 package org.kk.redpacktools.view;
 
-import android.app.Activity;
-import android.arch.core.executor.TaskExecutor;
-import android.os.Bundle;
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.kk.redpacktools.App;
 import org.kk.redpacktools.R;
 import org.kk.redpacktools.db.entities.RedPackLog;
-import org.w3c.dom.Text;
+import org.kk.redpacktools.utils.TimeUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,24 +28,80 @@ public class LogActivity extends BaseActivity {
     @BindView(R.id.tv_totalCount)
     TextView tv_totalCount;
 
+    @BindView(R.id.list_logs)
+    RecyclerView list_logs;
+
+    Adapter adapter;
+    private List<RedPackLog> mylogs = new ArrayList<>();
+
     @Override
     void initView() {
         setContentView(R.layout.activity_log);
         ButterKnife.bind(this);
+
+        list_logs.setAdapter(adapter = new Adapter(mContext, mylogs));
+        list_logs.setLayoutManager(new LinearLayoutManager(mContext));
     }
 
     @Override
     void initData() {
 
-        Flowable.fromCallable(()->
-            App.getDB().redPackDao().queryAll()
-        ).subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe((logs)->{
-            tv_totalCount.setText("总次数："+ logs.size());
+        Flowable.fromCallable(() ->
+                App.getDB().redPackDao().queryAll()
+        ).subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe((logs) -> {
+            tv_totalCount.setText("总次数：" + logs.size());
             double sum = 0;
-            for(RedPackLog log:logs) sum+=log.money;
-            tv_totalMoney.setText(String.format("总金额：%.2f",sum));
+            for (RedPackLog log : logs) sum += log.money;
+            tv_totalMoney.setText(String.format("总金额：%.2f", sum));
 
+            mylogs.clear();
+            mylogs.addAll(logs);
+            adapter.notifyDataSetChanged();
         });
 
     }
+
+    class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        private Context mContext;
+        private List<RedPackLog> redPackLogs;
+
+        public Adapter(Context context, List<RedPackLog> redPackLogs) {
+            this.mContext = context;
+            this.redPackLogs = redPackLogs;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_log, parent, false);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            RedPackLog redPackLog = redPackLogs.get(position);
+            holder.tv_msg.setText(String.format("%d 收到来自 %s 的红包：%.2f ", position + 1, redPackLog.name, redPackLog.money));
+            holder.tv_time.setText(TimeUtils.getDescriptionTimeFromTimestamp(redPackLog.time));
+        }
+
+        @Override
+        public int getItemCount() {
+            return redPackLogs.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            @BindView(R.id.tv_msg)
+            TextView tv_msg;
+            @BindView(R.id.tv_time)
+            TextView tv_time;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+        }
+    }
+
 }
